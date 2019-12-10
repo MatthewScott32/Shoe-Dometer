2e28a8a1d410f73b9e7d265f5f7f4184e3c23933
Matthew Blagg <matthewscottblagg@gmail.com>
2019-12-10 11:15:04 -0600

    file structure and navbar rendered

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,14 @@
+import { Route, withRouter, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+
+
+
+export default class ApplicationViews extends Component {
+    render() {
+     return(
+         <>
+
+        </>
+     )
+   }
+}
